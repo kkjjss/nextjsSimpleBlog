abce391cc626b90bcb66df8cfe3e12cd559d6cb9
jjsskk <jinsukk9763@gmail.com>
2022-09-21 18:32:14 +0900

    Style-component 정리

---
+++ styles/utils.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const BorderCircleImage = styled.div`
+    position: relative;
+    width: 144px;
+    height: 144px;
+    img {
+        background-color: #fcd358;
+        border-radius: 9999px;
+    }
+`;
+
+export const HeadingLg = styled.h2`
+    font-size: 1.5rem;
+    line-height: 1.4;
+    margin: 1rem 0;
+`;
+export const Heading2Xl = styled.h1`
+    font-size: 2.5rem;
+    line-height: 1.2;
+    font-weight: 800;
+    letter-spacing: -0.05rem;
+    margin: 1rem 0;
+`;
+
+export const HeadingXl = styled.h1`
+    font-size: 2rem;
+    line-height: 1.3;
+    font-weight: 800;
+    letter-spacing: -0.05rem;
+    margin: 1rem 0;
+`;
+
+export const HeadingMd = styled.section`
+    font-size: 1.2rem;
+    line-height: 1.5;
+`;
+
+export const ColorInherit = styled.a`
+    color: inherit;
+`;
+
+export const Padding1px = styled.div`
+    padding-top: 1px;
+`;
+
+export const List = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+`;
+
+export const ListItem = styled.li`
+    margin: 0 0 1.25rem;
+`;
+
+export const LightText = styled.div`
+    color: #666;
+`;
